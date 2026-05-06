e4486f6462df09de87e5c2e26864f31393971f4a
federicoacker <acker.federico@gmail.com>
2026-05-06 16:04:29 +0200

    Messa l'onChange agli input, adesso lavoro al submit

--- src/components/main/AddMovie.jsx
+++ src/components/main/AddMovie.jsx
@@ -5,23 +5,39 @@ function AddMovie({ movieArray, setMovieArray }) {
         {
             title: null,
             genre: null,
-            img: null
+            image: null
         }
     )
-    
+
+    const changeHandler = (event) => {
+        const target = event.target;
+        let value = target.value;
+        if(target.name === "image"){
+            value = URL.createObjectURL(target.files[0]);
+        }
+        setNewMovie(
+            {
+                ...newMovie,
+                [target.name]:value
+            }
+        )
+    }
+
     return (
-        <form className="form-control d-flex flex-column">
-            <label htmlFor="add-title">Titolo del film da aggiungere</label>
-            <input type="text" name="title" id="add-title" />
-            <label htmlFor="add-genre">Genere del film</label>
-            <select name="genre" id="add-genre">
+        <form className="form-control d-flex flex-column my-4" data-bs-theme="dark">
+            <label className="form-label" htmlFor="add-title">Titolo del film da aggiungere</label>
+            <input className="form-control my-2" type="text" name="title" id="add-title" value={newMovie.title} onChange={changeHandler}/>
+            <label className="form-label" htmlFor="add-genre">Genere del film</label>
+            <select className="form-control my-2" name="genre" id="add-genre" value={newMovie.genre} onChange={changeHandler}>
                 <option value="Fantascienza">Fantascienza</option>
                 <option value="Thriller">Thriller</option>
                 <option value="Romantico">Romantico</option>
                 <option value="Azione">Azione</option>
             </select>
-            <label htmlFor="add-image">Poster del film</label>
-            <input type="file" name="image" id="add-image"></input>
+            <label className="form-label" htmlFor="add-image">Poster del film</label>
+            <input className="form-control my-2" type="file" name="image" id="add-image" onChange={changeHandler}></input>
+            <button className="btn btn-primary">Aggiungi film</button>
+            <img src={newMovie.image} alt={newMovie.title}/>
         </form>
     )
 }
