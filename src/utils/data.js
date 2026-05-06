1231c1224b1610f35efb4c3ec80edf2c11f50a85
federicoacker <acker.federico@gmail.com>
2026-05-06 14:23:20 +0200

    Progetto iniziale con data.js in cui salvare l'array

---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+const movieArray = [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+];
+
+export { 
+    movieArray 
+}
